17486dbf3390dff50d635bf783babf4a68cb89d0
keilokimnida <tkl48leon@gmail.com>
2021-12-11 02:07:26 +0800

    Fix bugs

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -5,76 +5,82 @@ import ProfileIcon from './ProfileIcon';
 
 const ChatRoom = () => {
 
-    const messagesEndRef = useRef(null)
+    const messagesEndRef = useRef(null);
 
     const scrollToBottom = () => {
-      messagesEndRef.current?.scrollIntoView({ 
-        behavior: 'smooth',
-        block: 'end',
-        inline: 'nearest' })
-    }
-  
+        return messagesEndRef.current?.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end',
+            inline: 'nearest'
+        });
+    };
+
     useEffect(() => {
-      scrollToBottom()
+        scrollToBottom();
     }, []);
 
     return (
-        <div className = "c-Chat-room">
-            <div className = "c-Chat-room__Top">
-                <div className = "c-Top__Pfp">
+        <div className="c-Chat-room">
+            <div className="c-Chat-room__Top">
+                <div className="c-Top__Pfp">
                     <ProfileIcon />
                 </div>
-                <div className = 'c-Top__Details'>
+                <div className='c-Top__Details'>
                     <h1>@dlwlrma</h1>
                     <p>Online</p>
                 </div>
             </div>
-            <span className = "c-Divider c-Divider--horizontal"></span>
-            <div className = "c-Chat-room__Messages">
-                <p className = "c-Messages__Timestamp">November 26, 2021</p>
+            <span className="c-Divider c-Divider--horizontal"></span>
+            <div className="c-Chat-room__Messages">
+                <p className="c-Messages__Timestamp">November 26, 2021</p>
                 <ChatMessage
                     type={ENUMS.messageFrom.ME}
                     content="Yessirimnidaimnidadiamdianda sid asodj aosjdojas oijdo ajosidj osajo djsoajd oijaois djo"
-                    />
-                    <ChatMessage
+                />
+                <ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                     <ChatMessage
+                />
+                  <ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                     <ChatMessage
+                />
+                  <ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                     <ChatMessage
+                />
+                  <ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                     <ChatMessage
+                />
+
+<ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                     <ChatMessage
+                />
+                  <ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                     <ChatMessage
+                />
+                  <ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                     <ChatMessage
+                />
+                  <ChatMessage
                     type={ENUMS.messageFrom.FRIEND}
                     content="Yesir"
-                    />
-                    <span ref={messagesEndRef}/>
+                />
+                  <ChatMessage
+                    type={ENUMS.messageFrom.FRIEND}
+                    content="Yesir"
+                />
+                <span ref={messagesEndRef} />
             </div>
-            <div className = "c-Chat-room__Input">
-                <input type = "text" placeholder="Type something..."/>
+            <div className="c-Chat-room__Input">
+                <input type="text" placeholder="Type something..." />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ChatRoom;
